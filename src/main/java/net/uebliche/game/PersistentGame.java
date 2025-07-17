d794cdbe2fbea1ad86fc62725a9e3baecf61c01b
Freddi <mail@FrederikHeinrich.de>
2025-07-17 14:01:14 +0200

    refactor(game): update PersistentGame constructor for improved type safety

--- src/main/java/net/uebliche/game/PersistentGame.java
+++ src/main/java/net/uebliche/game/PersistentGame.java
@@ -1,10 +1,11 @@
 package net.uebliche.game;
 
+import net.uebliche.server.GamePlayer;
 import net.uebliche.server.GameServer;
 
 public abstract class PersistentGame<Settings extends PersistentGameSettings> extends Game<Settings> {
 
-    public PersistentGame(GameServer gameServer) {
+    public PersistentGame(GameServer<GamePlayer> gameServer) {
         super(gameServer);
     }
 
